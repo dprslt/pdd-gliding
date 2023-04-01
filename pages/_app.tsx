ae82182acc3aae167622cd874c540fa63bc1f9de
Theo Depresle <theo.depresle@gmail.com>
2023-04-01 14:49:51 +0200

    test SW on dev env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,30 @@ import '../styles/mobilePage.scss';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function () {
+                navigator.serviceWorker.register('/sw.js').then(
+                    function (registration) {
+                        console.log(
+                            'Service Worker registration successful with scope: ',
+                            registration.scope
+                        );
+                    },
+                    function (err) {
+                        console.log(
+                            'Service Worker registration failed: ',
+                            err
+                        );
+                    }
+                );
+            });
+        }
+    }, []);
+
     return (
         <>
             {process && process.env.NODE_ENV === 'production' && (
@@ -26,6 +48,24 @@ function MyApp({ Component, pageProps }: AppProps) {
                 </>
             )}
             <Head>
+                <link href="touch-icon-iphone.png" rel="apple-touch-icon" />
+                <link
+                    href="/icone-pdd-32_x_32.png"
+                    rel="apple-touch-icon"
+                    sizes="32x32"
+                />
+                <link
+                    href="/icone-pdd-128_x_128.png"
+                    rel="apple-touch-icon"
+                    sizes="128x128"
+                />
+                <link
+                    href="/icone-pdd-512_x_512.png"
+                    rel="apple-touch-icon"
+                    sizes="512x512"
+                />
+                <link rel="manifest" href="/dprslt-pdd.webmanifest" />
+                <meta name="theme-color" content="#a7001e" />
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1"
