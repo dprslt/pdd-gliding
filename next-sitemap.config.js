16408eb5c4f397dd6decb6008ff5ccc93f33ecae
Theo Depresle <theo.depresle@gmail.com>
2023-06-29 23:35:41 +0200

    Add sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://pdd.dprslt.fr',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+};
