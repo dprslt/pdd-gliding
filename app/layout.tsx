2535fefc512288f0c97eba571ccd1564cce6d752
dprslt <theo.depresle@gmail.com>
2024-03-16 20:08:34 +0100

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import '@fontsource/lato/700.css';
 import '@fontsource/baloo-2';
 import '../styles/globals.scss';
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function RootLayout({
     // Layouts must accept a children prop.
     // This will be populated with nested layouts or pages
@@ -34,6 +36,7 @@ export default function RootLayout({
             <body>
                 <div className="root-container">{children}</div>
             </body>
+            <Analytics />
         </html>
     );
 }
