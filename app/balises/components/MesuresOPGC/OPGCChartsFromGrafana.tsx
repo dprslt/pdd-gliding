9c779480c7f02baf81faba7ebaaba60de7da1478
dprslt <theo.depresle@gmail.com>
2024-03-16 23:45:23 +0100

    add wind directions on the scale

--- app/balises/components/MesuresOPGC/OPGCChartsFromGrafana.tsx
+++ app/balises/components/MesuresOPGC/OPGCChartsFromGrafana.tsx
@@ -5,7 +5,11 @@ import { BasicTooltip } from '@nivo/tooltip';
 import React from 'react';
 import { GraphData } from '../../../../services/opgc/apiGrafanaOPGC';
 import { DateTime } from 'luxon';
-import { convertDegToOrientation } from 'services/wind/OrientationMapper';
+import {
+    convertDegToOrientation,
+    numericHalfWindSegment,
+    windSegment,
+} from 'services/wind/OrientationMapper';
 
 type OPGCChartsFromGrafanaProps = {
     windData: GraphData;
@@ -87,7 +91,11 @@ const OPGCChartsFromGrafana: React.FC<OPGCChartsFromGrafanaProps> = ({
                     margin={{ top: 10, right: 50, bottom: 30, left: 10 }}
                     xScale={{ format: '%Y-%m-%dT%H:%M:%S.%L%Z', type: 'time' }}
                     xFormat="time:%Hh%M"
-                    yScale={{ type: 'linear', min: 0, max: 360 }}
+                    yScale={{
+                        type: 'linear',
+                        min: 0,
+                        max: 361,
+                    }}
                     yFormat=" >-d"
                     curve="monotoneX"
                     axisTop={null}
@@ -100,7 +108,11 @@ const OPGCChartsFromGrafana: React.FC<OPGCChartsFromGrafanaProps> = ({
                         tickSize: 5,
                         tickPadding: 5,
                         tickRotation: 0,
-                        format: '-d',
+                        // format: '-d',
+                        tickValues: numericHalfWindSegment,
+                        format: (v) => {
+                            return convertDegToOrientation(v);
+                        },
                         legend: 'Direction du vent',
                         legendOffset: 40,
                         legendPosition: 'middle',
--- services/wind/OrientationMapper.ts
+++ services/wind/OrientationMapper.ts
@@ -1,4 +1,4 @@
-const segments = [
+export const windSegment = [
     'N',
     'NNE',
     'NE',
@@ -17,7 +17,15 @@ const segments = [
     'NNO',
 ];
 
+export const numericWindSegment = Array(17)
+    .fill(0)
+    .map((a, i, arr) => i * 22.5);
+
+export const numericHalfWindSegment = Array(9)
+    .fill(0)
+    .map((a, i, arr) => i * 45);
+
 export function convertDegToOrientation(orientation: number): string {
     const segmentIndex = Math.floor((orientation + 11.25) / 22.5) % 16;
-    return segments[segmentIndex];
+    return windSegment[segmentIndex];
 }
