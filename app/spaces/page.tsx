650c4e78d3eec2c8e4a164edfb558c4ea348d294
dprslt <theo.depresle@gmail.com>
2024-03-17 23:18:29 +0100

    Add first notam fetches

--- app/spaces/page.tsx
+++ app/spaces/page.tsx
@@ -7,23 +7,14 @@ import SofiaBriefingLink from './components/LinkSofia';
 
 import spacesStyles from './spaces.module.scss';
 import ParsedNotams from './components/ParsedNotams';
+import {
+    fetchNOTAMForRoute,
+    PDDNorthSouthRoute,
+} from 'services/spaces/sofiaNotam';
+import NotamCard from './components/notams/NotamCard';
 
 export default async function Page() {
-    const coockiesInFetch = await fetch(
-        `https://sofia-briefing.aviation-civile.gouv.fr/sofia/pages/homepage.html`,
-        { cache: 'no-store' }
-    );
-
-    // const buildParams = `%3Aoperation=postNarrowRoutePibRequest&valid_from=2024-02-06T22%3A00%3A16Z&duration=1200&traffic=V&fl_lower=0&fl_upper=85&width=30&radiusAD=30&route%5B%5D=LFBK&route%5B%5D=LFLW&&isFromSofia=true&operation=postNarrowRoutePibRequest&target=%23aside-target&href=%2Fsofia%2Fpages%2Fnotamroute.html&typeVol=N&departure_date=06-02-2024&departure_time=2300&lang=fr&routeVal=false`;
-
-    // const result = await fetch(
-    //     'https://sofia-briefing.aviation-civile.gouv.fr/sofia',
-    //     {
-    //         method: 'POST',
-    //         body: buildParams,
-    //         credentials: 'same-origin',
-    //     }
-    // );
+    const notams = await fetchNOTAMForRoute(PDDNorthSouthRoute);
 
     return (
         <AppPage
@@ -31,6 +22,16 @@ export default async function Page() {
             pageIcon={faPlaneCircleCheck}
             className={spacesStyles['space-tab-page']}
         >
+            <div>
+                <h2>NOTAMS: "Airspace Restrictions"</h2>
+
+                {notams
+                    .filter((notam) => notam.qLine.code23.startsWith('R'))
+                    .map((n) => {
+                        return <NotamCard key={n.id} notam={n} />;
+                    })}
+            </div>
+
             <TmaInfos />
             <RtbaMapLink />
             <SofiaBriefingLink />
---
+++ services/spaces/sofiaNotam.ts
@@ -0,0 +1,178 @@
+import { DateTime } from 'luxon';
+import { URLSearchParams } from 'url';
+import { v4 } from 'uuid';
+import { cookies } from 'next/headers';
+
+export type SofiaNotamRouteParams = {
+    ':operation': string;
+    duration: string;
+    traffic: string;
+    fl_lower: string;
+    fl_upper: string;
+    width: string;
+    radiusAD: string;
+    route: Array<string>;
+    lang: string;
+    routeVal: string;
+};
+export type SofiaNotamTimeParams = {
+    valid_from: string;
+    departure_date: string;
+    departure_time: string;
+};
+export type SofiaNotamQueryParams = SofiaNotamRouteParams &
+    SofiaNotamTimeParams;
+
+export const PDDNorthSouthRoute = {
+    ':operation': 'postNarrowRoutePibRequest',
+    operation: 'postNarrowRoutePibRequest',
+    duration: '2400',
+    traffic: 'V',
+    fl_lower: '0',
+    fl_upper: '85',
+    width: '30',
+    radiusAD: '30',
+    route: ['LFBK', 'LFLW'],
+    isFromSofia: true,
+    target: '#aside-target',
+    href: '/sofia/pages/notamroute.html',
+    typeVol: 'N',
+    lang: 'fr',
+    routeVal: 'false',
+    // valid_from: '2024-03-18T22:00:16Z',
+    // departure_date: '19-03-2024',
+    // departure_time: '1200',
+};
+
+export type NOTAMstructure = {
+    pibSection: string;
+    id: string;
+    nof: string;
+    series: string;
+    number: number;
+    yean: number;
+    type: string;
+    qLine: {
+        fir: string;
+        code23: string;
+        code45: string;
+        traffic: string;
+        purpose: string;
+        scope: string;
+        lower: number;
+        upper: number;
+    };
+    radius: number;
+
+    coordinates: string;
+    itemA: string;
+    startValidity: string;
+    endValidity: string;
+    startValidityFormat: string;
+    endValidityFormat: string;
+    itemE: string;
+    multiLanguage: {
+        itemE: string;
+    };
+    itemF?: string;
+    itemG?: string;
+    marker: string;
+    valid: string;
+};
+
+export async function fetchNOTAMForRoute(
+    config: SofiaNotamRouteParams
+): Promise<Array<NOTAMstructure>> {
+    const date = DateTime.now().startOf('hour');
+
+    const fullConfig: SofiaNotamQueryParams = {
+        ...config,
+        valid_from: date.toUTC().toISO({
+            suppressMilliseconds: true,
+            extendedZone: false,
+        }),
+        departure_date: date.toFormat('dd-MM-yyyy'),
+        departure_time: date.toFormat('HHmm'),
+        // uuid: v4(),
+    };
+
+    console.log(fullConfig);
+
+    const { route, ...otherParams } = fullConfig;
+
+    const urlParams = new URLSearchParams(otherParams);
+    route.forEach((point) => urlParams.append('route[]', point));
+
+    console.log(urlParams.toString());
+
+    const headers = new Headers();
+    headers.append('Accept', 'application/json, text/javascript, */*; q=0.01');
+    headers.append(
+        'Referer',
+        'https://sofia-briefing.aviation-civile.gouv.fr/sofia/pages/notamform.html'
+    );
+    headers.append('X-Requested-With', 'XMLHttpRequest');
+    headers.append('Origin', 'https://sofia-briefing.aviation-civile.gouv.fr');
+    headers.append(
+        'Content-Type',
+        'application/x-www-form-urlencoded; charset=UTF-8'
+    );
+
+    // get a cookie for the session
+    const r = await fetch(
+        `https://sofia-briefing.aviation-civile.gouv.fr/sofia/pages/homepage.html`,
+        {
+            credentials: 'include',
+            headers,
+            next: {
+                revalidate: 1000,
+            },
+        }
+    );
+
+    const cookie = r.headers.getSetCookie()[0];
+
+    headers.append('Cookie', cookie);
+
+    const notamsRequest = await fetch(
+        `https://sofia-briefing.aviation-civile.gouv.fr/sofia`,
+        {
+            method: 'POST',
+            body: urlParams.toString(),
+            headers,
+            credentials: 'include',
+            next: {
+                revalidate: 1000,
+            },
+        }
+    );
+
+    const response = await notamsRequest.json();
+
+    const notams = JSON.parse(response['status.message']);
+
+    // https://www.notams.faa.gov/common/qcode/qcode.html
+
+    const FIRNOTAMS: Array<NOTAMstructure> = [];
+
+    Object.entries(notams['listnotams']['FIR']).forEach(([notamCat, cat]) => {
+        cat.forEach((notamImpacted) => {
+            notamImpacted['sortedNotamsByImpactedAerodromes']?.forEach(
+                (aerodromes) => {
+                    aerodromes.sortedNotamsByPurpose.forEach((purpose) => {
+                        FIRNOTAMS.push(...purpose.notam);
+                    });
+                }
+            );
+            // console.log(JSON.stringify(notamImpated, null, 2));
+        });
+    });
+
+    // console.log(FIRNOTAMS);
+
+    // const notamResponse = await notamsRequest.json();
+
+    // console.log(notamResponse);
+
+    return FIRNOTAMS;
+}
