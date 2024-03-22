9254b4a44faddf61648c0eda819fdb41d477f3da
dprslt <theo.depresle@gmail.com>
2024-03-23 00:56:57 +0100

    WRITE FILE SYNC

--- copy-certs.js
+++ copy-certs.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
-fs.createReadStream('./certs/DGAC.pem').pipe(
-    fs.createWriteStream('/vercel/output/DGAC.pem')
-);
+const certs = fs.readFileSync('./certs/DGAC.pem');
+console.log(certs);
+fs.writeFileSync('./.next/DGAC.pem', certs);
+
+// fs.createReadStream('./certs/DGAC.pem').pipe(
+//     fs.createWriteStream('./.next/DGAC.pem')
+// );
 // const cert = fs.readFile('./certs/DGAC.pem', (data) => {
 //     fs.writeFile()
 // })
