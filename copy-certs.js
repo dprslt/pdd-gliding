fc0a9f07033005d906f884828348e47ff1655c56
dprslt <theo.depresle@gmail.com>
2024-03-23 00:36:44 +0100

    add hardcoded value for vercel

--- copy-certs.js
+++ copy-certs.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 fs.createReadStream('./certs/DGAC.pem').pipe(
-    fs.createWriteStream('./.next/DGAC.pem')
+    fs.createWriteStream('/vercel/output/DGAC.pem')
 );
 // const cert = fs.readFile('./certs/DGAC.pem', (data) => {
 //     fs.writeFile()
