bec95ac1672bdaa8b71132d91fd01e72f8e52cfd
dprslt <theo.depresle@gmail.com>
2024-04-20 19:14:25 +0200

    try to use a permanent redirect

--- app/spaces/page.tsx
+++ app/spaces/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from 'next/navigation';
+import { permanentRedirect } from 'next/navigation';
 
 export default async function Page() {
-    redirect('spaces/notams');
+    permanentRedirect('spaces/notams');
     return null;
 }
