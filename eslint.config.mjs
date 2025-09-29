5b16fe60c432446d04725d9ff5b90e2cd9f2f9c0
dprslt <theo.depresle@gmail.com>
2025-09-29 22:16:38 +0200

    Fix the prettier and eslint coniguration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,20 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
 });
 
-export default defineConfig([
+export default [
     {
-        ignores: ['node_modules/**', '.next/**']
+        ignores: ['node_modules/**', '.next/**', 'public/**', '*.config.*', '.yarn/**']
     },
-    {
-        // TODO: add next/typescript but for now their it too many errors: ~9100
-        extends: compat.extends("next/core-web-vitals"),
-    },
-    {
-        files: ['**/*.tsx'],
-        rules: {
-            '@typescript-eslint/no-empty-object-type': 'off'
-        }
-    }
-]);
+    ...compat.extends("next/core-web-vitals")
+];
